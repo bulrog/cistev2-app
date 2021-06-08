ed7bbd050f15111f7e3de0b86eab16d93e147794
Morel Jerome <jem@melexis.com>
2021-06-09 00:14:19 +0200

    master: add start screen and music and changed launched icon

--- app/src/main/java/com/bulrog59/ciste2dot0/MainActivity.kt
+++ app/src/main/java/com/bulrog59/ciste2dot0/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.bulrog59.ciste2dot0
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val intent = Intent(this@MainActivity, BackgroundSoundService::class.java)
+        startService(intent)
         setContentView(R.layout.activity_main)
     }
 }
