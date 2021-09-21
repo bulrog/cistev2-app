c6c939d141436e56902d2127bc6495fda7943e75
Morel Jerome <jem@melexis.com>
2021-09-21 22:34:41 +0200

    editor: finish the view for the creation of a new game.

--- app/src/main/java/com/bulrog59/ciste2dot0/GameMgtActivity.kt
+++ app/src/main/java/com/bulrog59/ciste2dot0/GameMgtActivity.kt
@@ -1,12 +1,16 @@
 package com.bulrog59.ciste2dot0
 
 import android.content.pm.ActivityInfo
+import android.content.res.Resources
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bulrog59.ciste2dot0.game.management.GameListAdapter
+import java.util.*
 
 class GameMgtActivity : AppCompatActivity() {
 
@@ -24,6 +28,17 @@ class GameMgtActivity : AppCompatActivity() {
         }
         findViewById<ImageButton>(R.id.new_game).setOnClickListener {
             setContentView(R.layout.editor_new_game)
+            val languages =
+                HashSet(Locale.getAvailableLocales().map { it.displayLanguage }).sorted()
+            findViewById<AutoCompleteTextView>(R.id.game_language_input).setAdapter(
+                ArrayAdapter(
+                    this,
+                    android.R.layout.simple_dropdown_item_1line,
+                    languages
+                )
+            )
+
+
         }
 
     }
