6380dcdbed556ade5fa9a168c5bc63e1d811c69f
Morel Jerome <jem@melexis.com>
2021-09-23 21:59:44 +0200

    editor: fix navigation issue on game creation.

--- app/src/main/java/com/bulrog59/ciste2dot0/GameMgtActivity.kt
+++ app/src/main/java/com/bulrog59/ciste2dot0/GameMgtActivity.kt
@@ -21,10 +21,8 @@ import kotlin.collections.ArrayList
 
 class GameMgtActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Thread.setDefaultUncaughtExceptionHandler(ExceptionHandler(this));
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+
+    private fun gameSelectionScreen() {
         setContentView(R.layout.game_management)
         val recyclerView = findViewById<RecyclerView>(R.id.games_list)
         recyclerView.adapter = GameListAdapter(this)
@@ -34,43 +32,57 @@ class GameMgtActivity : AppCompatActivity() {
             System.exit(0)
         }
         findViewById<ImageButton>(R.id.new_game).setOnClickListener {
-            setContentView(R.layout.editor_new_game)
-            val languages =
-                HashSet(Locale.getAvailableLocales().map { it.displayLanguage }).sorted()
-            findViewById<AutoCompleteTextView>(R.id.game_language_input).setAdapter(
-                ArrayAdapter(
-                    this,
-                    android.R.layout.simple_dropdown_item_1line,
-                    languages
-                )
-            )
-            findViewById<Button>(R.id.create_game).setOnClickListener {
-                val name = findViewById<TextView>(R.id.game_title_input).text.toString()
-                val language = findViewById<TextView>(R.id.game_language_input).text.toString()
-                val description =
-                    findViewById<TextView>(R.id.game_description_input).text.toString()
-                val location = findViewById<TextView>(R.id.game_location_input).text.toString()
-                val id = UUID.randomUUID()
-                val gameMetaData = GameMetaData(
-                    name = name,
-                    language = language,
-                    description = description,
-                    location = location,
-                    id = id,
-                    sizeInMB = null
-                )
-                val gameData = GameData(
-                    scenes = listOf(SceneData(0, SceneType.exit, ObjectMapper().createObjectNode())),
-                    gameMetaData = gameMetaData,
-                    backButtonScene = 0,
-                    starting = 0
-                )
-                GameDataManager(this).createGame(gameData)
-
-            }
-
-
+            gameCreationScreen()
         }
+    }
+
+    private fun gameCreationScreen() {
+        setContentView(R.layout.editor_new_game)
+        val languages =
+            HashSet(Locale.getAvailableLocales().map { it.displayLanguage }).sorted()
+        findViewById<AutoCompleteTextView>(R.id.game_language_input).setAdapter(
+            ArrayAdapter(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                languages
+            )
+        )
+        findViewById<Button>(R.id.create_game).setOnClickListener {
+            val name = findViewById<TextView>(R.id.game_title_input).text.toString()
+            val language = findViewById<TextView>(R.id.game_language_input).text.toString()
+            val description =
+                findViewById<TextView>(R.id.game_description_input).text.toString()
+            val location = findViewById<TextView>(R.id.game_location_input).text.toString()
+            val id = UUID.randomUUID()
+            val gameMetaData = GameMetaData(
+                name = name,
+                language = language,
+                description = description,
+                location = location,
+                id = id,
+                sizeInMB = null
+            )
+            val gameData = GameData(
+                scenes = listOf(SceneData(0, SceneType.exit, ObjectMapper().createObjectNode())),
+                gameMetaData = gameMetaData,
+                backButtonScene = 0,
+                starting = 0
+            )
+            GameDataManager(this).createGame(gameData)
+            gameSelectionScreen()
+        }
+    }
+
+    override fun onBackPressed() {
+       gameSelectionScreen()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.setDefaultUncaughtExceptionHandler(ExceptionHandler(this));
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+        gameSelectionScreen()
+
 
     }
 }
