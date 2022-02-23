85cfb262da9728feb92aec311a969c62f9cd0240
Morel Jerome <jem@melexis.com>
2022-02-23 23:08:45 +0100

    master: add capability to only accept once

--- app/src/main/java/com/bulrog59/ciste2dot0/DisclaimerActivity.kt
+++ app/src/main/java/com/bulrog59/ciste2dot0/DisclaimerActivity.kt
@@ -1,6 +1,7 @@
 package com.bulrog59.ciste2dot0
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -10,27 +11,50 @@ import kotlin.system.exitProcess
 
 class DisclaimerActivity : AppCompatActivity() {
 
+    private lateinit var pref: SharedPreferences
+
+    companion object {
+        val CISTE_PREF = "ciste_pref"
+        val APPROVED_DISCLAIMER = "approved_disclaimer"
+    }
+
+    private fun approveDisclaimer() {
+        pref.edit().putBoolean(APPROVED_DISCLAIMER, true).apply()
+        launchedLoginActivity()
+    }
+
+    private fun launchedLoginActivity() {
+        startActivity(Intent(this, SignInActivity::class.java))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.disclaimer)
+        pref = getSharedPreferences(CISTE_PREF, 0)
+        if (!pref.getBoolean(APPROVED_DISCLAIMER, false)) {
+            setContentView(R.layout.disclaimer)
 
-        val resourceId = if (Locale.getDefault().equals(Locale.FRENCH)) {
-            R.raw.disclaimer_fr
+            val resourceId = if (Locale.getDefault().equals(Locale.FRENCH)) {
+                R.raw.disclaimer_fr
+            } else {
+                R.raw.disclaimer_en
+            }
+
+            findViewById<TextView>(R.id.disclaimer_text).text =
+                resources.openRawResource(resourceId).bufferedReader().use { it.readText() }
+
+            findViewById<Button>(R.id.accept_button).setOnClickListener {
+                approveDisclaimer()
+            }
+
+            findViewById<Button>(R.id.decline_button).setOnClickListener {
+                finish()
+                exitProcess(0)
+            }
         } else {
-            R.raw.disclaimer_en
+            launchedLoginActivity()
+
         }
 
-        findViewById<TextView>(R.id.disclaimer_text).text =
-            resources.openRawResource(resourceId).bufferedReader().use { it.readText() }
-
-        findViewById<Button>(R.id.accept_button).setOnClickListener {
-            startActivity(Intent(this, SignInActivity::class.java))
-        }
-
-        findViewById<Button>(R.id.decline_button).setOnClickListener {
-            finish()
-            exitProcess(0)
-        }
     }
-}
 
+}
